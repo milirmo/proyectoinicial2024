b966fca1a5e9c093a939f9d879876cfc08652d74
milirmo <milagritosromero.mr@gmail.com>
2024-08-20 10:58:14 -0300

    vinculacion del modal para tomar datos de un producto seleccionado; modal para editar productos ya agregados

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -1,61 +1,126 @@
 import { Component } from '@angular/core';
-
-import { Producto } from 'src/app/models/producto'; //importo interfaz producto
-import { CrudService } from '../../service/crud.service'; //importo servicip crud
-import { FormControl, FormGroup, Validators } from '@angular/forms'; //importo form controlm form engrupar y validatos (permite marcar errores)
+import { Producto } from 'src/app/models/producto';
+import { CrudService } from '../../service/crud.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-
 export class TableComponent {
+  // Creamos colección local de productos -> la definimos como array
+  coleccionProductos: Producto[] = [];
 
-  //creamos coleccion local del productos y la definimo como arreglo (array)
-  coleccionProductos: Producto[]= [];
+  productoSeleccionado!: Producto; // ! <- tomar valores vacíos
 
-  //definimos formulario para los productos
-  /*
-  los datos se inicializan en:
-  string= ''
-  number= 0
-  */
+  modalVisibleProducto: boolean = false;
+
+  // Definimos formulario para los productos
+  /**
+   * Atributos alfanuméricos (string) se inicializan con comillas simples
+   * Atributos numéricos (number) se inicializan con cero ('0')
+   */
   producto = new FormGroup({
     nombre: new FormControl('', Validators.required),
     precio: new FormControl(0, Validators.required),
-    categoria: new FormControl('', Validators.required),
     descripcion: new FormControl('', Validators.required),
+    categoria: new FormControl('', Validators.required),
     imagen: new FormControl('', Validators.required),
     alt: new FormControl('', Validators.required)
   })
+  constructor(public servicioCrud: CrudService) { }
 
-  constructor(public servicioCrud: CrudService){}
-
-  ngOnInit(): void{}
-
-  async agregarProducto(){
-
-    if(this.producto.valid){
+  ngOnInit(): void {
+    // subscribe -> método de notificación de cambios (observable)
+    this.servicioCrud.obtenerProducto().subscribe(producto => {
+      this.coleccionProductos = producto;
+    })
+  }
+  async agregarProducto() {
+    if (this.producto.valid) {
       let nuevoProducto: Producto = {
         idProducto: '',
         nombre: this.producto.value.nombre!,
         precio: this.producto.value.precio!,
-        categoria: this.producto.value.categoria!,
         descripcion: this.producto.value.descripcion!,
+        categoria: this.producto.value.categoria!,
         imagen: this.producto.value.imagen!,
-        alt: this.producto.value.alt!,
+        alt: this.producto.value.alt!
       }
-
       await this.servicioCrud.crearProducto(nuevoProducto)
-    .then (producto=> {
-      alert("¡Nuevo producto agregado! :)")
-    })
-    .catch (error=> {
-      alert("Ocurrió un error al intentar agregar un nuevo producto...")
-    })
+        .then(producto => {
 
-    };
+          alert("Ha agregado un nuevo producto con éxito.");
+
+          //resetea el formulario y queda vacío
+          this.producto.reset
+        })
+
+        .catch(error => {
+
+          alert("Ha ocurrido un error al cargar un producto. /n"+error);
+
+          this.producto.reset
+        })
+    }
   }
 
+  // función vinculada al modal y el botón de la tabla
+  mostrarBorrar(productoSeleccionado: Producto){
+    this.modalVisibleProducto = true;
+
+    this.productoSeleccionado = productoSeleccionado;
+  }
+
+  borrarProducto(){
+    this.servicioCrud.eliminarProducto(this.productoSeleccionado.idProducto)
+    .then(respuesta => {
+      alert("Se ha podido eliminar con éxito.");
+    })
+    .catch(error => {
+      alert("Ha ocurrido un error al eliminar un producto: \n"+error);
+    })
+  }
+
+  //EDITAR PRODUCTOS
+  mostrarEditar(productoSeleccionado: Producto){
+
+    this.productoSeleccionado = productoSeleccionado;
+
+    //toma valores del producto seleccionado y los autocompleta en el form del modal (menos id)
+    this.producto.setValue({
+      nombre: productoSeleccionado.nombre,
+      precio: productoSeleccionado.precio,
+      descripcion: productoSeleccionado.descripcion,
+      categoria: productoSeleccionado.categoria,
+      imagen: productoSeleccionado.imagen,
+      alt: productoSeleccionado.alt
+    })
+  }
+
+  editarProducto(){
+    let datos: Producto = {
+
+      //Solo idProducto no se modifica por el usuario (por eso usamos productoSeleccionado)
+      idProducto: this.productoSeleccionado.idProducto,
+      //Los demás atributos reciben nueva información desde el formulario
+      nombre: this.producto.value.nombre!,
+      precio: this.producto.value.precio!,
+      descripcion: this.producto.value.descripcion!,
+      categoria: this.producto.value.categoria!,
+      imagen: this.producto.value.imagen!,
+      alt: this.producto.value.alt!,
+    }
+
+    //Enviamos al método el id del producto seleccionado y los datos actualizados
+    this.servicioCrud.modificarProducto(this.productoSeleccionado.idProducto, datos)
+    .then(producto => {
+      alert("¡Producto modificado con éxito! =)")
+    })
+
+  .catch(error => {
+    alert("Ocurrió un problema al intentar modificar el producto... /n"+error)
+  })
+  }
 }
